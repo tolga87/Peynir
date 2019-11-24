f88acc87f71529dfd7e3eea18e00d1278d72581f
tolga87 <tolga.ceng@gmail.com>
2019-11-24 15:41:25 -0800

    Initial Commit

---
+++ Peynir/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Peynir
+//
+//  Created by tolga on 11/24/19.
+//  Copyright © 2019 Tolga AKIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
